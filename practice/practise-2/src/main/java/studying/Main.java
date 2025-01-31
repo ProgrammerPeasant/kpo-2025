b6c988f4eadb8f34c4c3a6f37c9f12d5577ebda0
ProgrammerPeasant <poland-fish@yandex.ru>
2025-01-31 11:51:30 +0300

    added new flying car type && flying engine

--- practice/practise-2/src/main/java/studying/Main.java
+++ practice/practise-2/src/main/java/studying/Main.java
@@ -4,9 +4,11 @@ import studying.domains.Car;
 import studying.domains.Customer;
 import studying.domains.HandEngine;
 import studying.domains.PedalEngine;
+import studying.factories.FlyingCarFactory;
 import studying.factories.HandCarFactory;
 import studying.factories.PedalCarFactory;
 import studying.params.EmptyEngineParams;
+import studying.params.FlyingEngineParams;
 import studying.params.PedalEngineParams;
 import studying.services.CarService;
 import studying.services.CustomerStorage;
@@ -24,21 +26,25 @@ public class Main {
         HseCarService hseCarService = new HseCarService(carService, customerStorage);
         PedalCarFactory pedalCarFactory = new PedalCarFactory();
         HandCarFactory handCarFactory = new HandCarFactory();
+        FlyingCarFactory flyingCarFactory = new FlyingCarFactory();
 
-        Customer first = new Customer("fisrt", 6, 4);
-        Customer second = new Customer("second", 4, 6);
-        Customer third = new Customer("third", 6, 6);
-        Customer fourth = new Customer("fourth", 4, 4);
+        Customer first = new Customer("fisrt", 6, 4, 120);
+        Customer second = new Customer("second", 4, 6, 150);
+        Customer third = new Customer("third", 6, 6, 90);
+        Customer fourth = new Customer("fourth", 4, 4, 100);
+        Customer fifth = new Customer("fifth", 2, 2, 320);
 
         customerStorage.addCustomer(first);
         customerStorage.addCustomer(second);
         customerStorage.addCustomer(third);
         customerStorage.addCustomer(fourth);
+        customerStorage.addCustomer(fifth);
 
         carService.addCar(pedalCarFactory, new PedalEngineParams(5));
         carService.addCar(pedalCarFactory, new PedalEngineParams(6));
         carService.addCar(handCarFactory, new EmptyEngineParams());
         carService.addCar(handCarFactory, new EmptyEngineParams());
+        carService.addCar(flyingCarFactory, new FlyingEngineParams());
 
         customerStorage.getCustomers().stream().map(Customer::toString).forEach(System.out::println);
         hseCarService.sellCars();
---
+++ practice/practise-2/src/main/java/studying/params/FlyingEngineParams.java
@@ -0,0 +1,5 @@
+package studying.params;
+
+public record FlyingEngineParams() {
+    public static final FlyingEngineParams DEFAULT = new FlyingEngineParams();
+}
